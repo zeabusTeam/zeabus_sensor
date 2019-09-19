6ada15dd20f474437bbb65fc760a283dfb3ea1a2
Supasan Komonlit <supasan_224@hotmail.com>
2019-09-19 18:26:45 +0700

    Include file for imu

---
+++ src/imu_node.cpp
@@ -0,0 +1,32 @@
+// FILE			: imu_node.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, September 19 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#include    <vector>
+
+#include    <iostream>
+
+#include    <ros/ros.h>
+
+#include    <zeabus/ros/node.hpp>
+
+#include    <zeabus/sensor/imu/connector.hpp>
+
+#include    <zeabus/sensor/imu/LORD_IMU_COMMUNICATION.hpp>
+
+
+int main( int argv , char** argc )
+{
+
+}
