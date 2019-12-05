6a98e63258f0bb8e847320ab38eb906d64684412
SupasanKomonlit <supasan_224@hotmail.com>
2019-12-05 13:07:15 +0700

    Add filer by variance rule

--- src/pressure_buffer.cpp
+++ src/pressure_buffer.cpp
@@ -10,6 +10,8 @@
 // REFERENCE
 
 // MACRO SET
+#define SIZE_DATA 5
+#define LIMIT_VARIANCE 1.0
 
 // MACRO CONDITION
 
@@ -22,6 +24,9 @@
 #include    <zeabus_utility/HeaderFloat64.h>
 
 #include    <zeabus/ros/node.hpp>
+
+float variance_rule( float data , bool setup = false);
+float calculate_variance( float* list_data , float mean );
 
 int main( int argv , char** argc )
 {
@@ -54,13 +59,29 @@ int main( int argv , char** argc )
 
     ros::Rate rate( frequency );
 
+full_buffer:
+    while( ros::ok() )
+    {
+        rate.sleep();
+        if( client_pressure.call( service_pressure ) )
+        {
+            variance_rule( -1.0 * service_pressure.response.depth , true );
+        }
+        else
+        {
+            ROS_ERROR( "Failure client to call topic for pressure sensor data : Full Buffer mode");
+        }
+        
+    }
+
+active_main:
     while( ros::ok() )
     {
         rate.sleep();
         if( client_pressure.call( service_pressure ) )
         {
             message_output.header = service_pressure.response.header;
-            message_output.data = -1.0 * service_pressure.response.depth;
+            message_output.data = variance_rule( -1.0 * service_pressure.response.depth );
             publisher_pressure.publish( message_output );
         }
         else
@@ -70,4 +91,44 @@ int main( int argv , char** argc )
         
     }
 
+}
+
+float variance_rule( float data , bool setup )
+{
+    float answer = data;
+    static float list_data[ SIZE_DATA ];
+    static unsigned int run = 0;
+    static float summation = 0;
+    if( setup )
+    {
+        for( unsigned int count = 0 ; count < SIZE_DATA ; count++ )
+        {
+            list_data[ count ] = data;
+            summation += list_data[ count ];
+        }
+    }
+    else if( calculate_variance( list_data , summation / SIZE_DATA ) > LIMIT_VARIANCE )
+    {
+            std::cout   << "Disagree data " << data << " because make over limit variance\n";
+            answer = summation / SIZE_DATA;
+    }
+    else
+    {
+        summation -= list_data[ run ];
+        list_data[ run ] = data;
+        summation += list_data[ run ];
+        answer = summation / SIZE_DATA;
+    }
+    return answer;
+} // function variance_rule
+
+float calculate_variance( float* list_data , float mean )
+{
+    float answer = 0;
+    for( unsigned int count = 0 ; count < SIZE_DATA ; count++ )
+    {
+        float temp = list_data[ count ] - mean;
+        answer += temp * temp;
+    }
+    return answer / SIZE_DATA;
 }
